6fde10b6a042fffaa14bd998452315eae530ab39
ruben-rebelo <ruben.rato96@gmail.com>
2025-01-29 12:06:03 +0000

    Working example

--- App.tsx
+++ App.tsx
@@ -1,35 +1,10 @@
 import * as React from 'react';
 import 'react-native-reanimated';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {InformationalScreenExample} from './app/InformationalScreen';
-import {TamaguiProvider} from '@tamagui/core';
-import {config} from './tamagui.config';
 import {AppRegistry} from 'react-native';
 import TextCarousel from './app/TextCarousel';
 
-const Stack = createNativeStackNavigator();
-
-function RootStack() {
-    return (
-        <Stack.Navigator>
-            <Stack.Screen
-                name="Home"
-                component={InformationalScreenExample}
-            />
-        </Stack.Navigator>
-    );
-}
-
 export default function App() {
-    return (
-        <TamaguiProvider
-            config={config}
-            defaultTheme="light"
-        >
-            <TextCarousel />
-        </TamaguiProvider>
-    );
+    return <TextCarousel />;
 }
 
 AppRegistry.registerComponent('App', () => App);
